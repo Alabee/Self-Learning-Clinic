93d0e9c700b1e9ef92dd217ca1726356eaf9f73a
Alabee <ianmutevu96@gmail.com>
2017-04-06 09:23:17 +0300

    Checks difference in elements in 2 lists

--- Day 4/Missing_number.py
+++ Day 4/Missing_number.py
@@ -1,11 +1,14 @@
 def find_missing(lista, listb):
-    if type(lista) is list:
-        if type(listb) is list:
-            result = set(listb) - set(lista)
-            if result == set():
+    
+    if type(lista) is list: #Checks if input given is a list
+        if type(listb) is list: #Checks if input given is a list
+            
+            result = set(listb) - set(lista) #Finds the difference of elements in the two lists
+            
+            if result == set(): # returns 0 if the lists have the same elements or the an empty list was given
                 return(0)
 
             else:
-                for data in result:
+                for data in result:#returns the missing number(s)
                     return data
 
